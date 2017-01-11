5ca3eb1f36415c505b8e81fc280c3003a6bccedc
Alex <rexforab@gmail.com>
2017-01-11 15:25:25 -0500

    Location and Camera

--- WinSetupDemo/WinSetupDemo/App.cs
+++ WinSetupDemo/WinSetupDemo/App.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Plugin.Geolocator;
+using Plugin.Media;
 using Xamarin.Forms;
 
 namespace WinSetupDemo
@@ -11,33 +12,105 @@ namespace WinSetupDemo
     {
         public App()
         {
-            Button btn = new Button();
-            btn.Text = "Hit me";
+            // Page controls
+            Label lblLocation = new Label() { FontSize = 20 };
+            Button btnLocation = new Button() { Text = "Get Location" };
+            Button btnTakePhoto = new Button() { Text = "Take Photo" };
+            Button btnGetPhoto = new Button() { Text = "Pick Photo" };
 
-            btn.Clicked += (sender, args) =>
-            {
-                btn.Text = "ARG YOU HIT ME";
-            };
-
-            // The root page of your application
+            // Page structure (root page of application)
             var content = new ContentPage
             {
-                Title = "WinSetupDemo",
+                Title = "Xamarin Demo",
                 Content = new StackLayout
                 {
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
                         new Label {
                             HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
+                            Text = "Xamarin Demo"
                         },
-                        btn
+                        btnLocation,
+                        lblLocation,
+                        btnTakePhoto,
+                        btnGetPhoto
                     }
                 }
             };
 
+            #region Button Events
+
+            // Camera
+            // --------------------------------
+
+            btnTakePhoto.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    content.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Test",
+                    Name = "test.jpg",
+                    SaveToAlbum = true
+                });
+
+                if (file == null)
+                    return;
+
+                await content.DisplayAlert("File Location", file.Path, "OK");
+
+                var image = new Image()
+                {
+                    Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = file.GetStream();
+                        file.Dispose();
+                        return stream;
+                    })
+                };
+
+                //or:
+                //image.Source = ImageSource.FromFile(file.Path);
+                //image.Dispose();
+            };
+
+            btnGetPhoto.Clicked += async (sender, args) => 
+            { 
+                var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    
+                });
+            };
+
+            //---------------------------------
+
+            // Location
+            // ---------------------
+
+            btnLocation.Clicked += async (sender, args) =>
+            {
+                var locator = CrossGeolocator.Current;
+
+                locator.DesiredAccuracy = 100; //100 is new default
+
+                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                lblLocation.Text = $"Lat: {position.Latitude} Long: {position.Longitude}";
+            };
+            // -------------------------------
+
+            #endregion
+
             MainPage = new NavigationPage(content);
         }
+
+        #region App Events
+        
 
         protected override void OnStart()
         {
@@ -53,5 +126,7 @@ namespace WinSetupDemo
         {
             // Handle when your app resumes
         }
+
+        #endregion
     }
 }
